91af31ac9c9c77e97e86f443eb2f0dd5e177542c
Alexandr <jsprog2017@yandex.ru>
2021-01-19 10:25:07 +0300

    add aliases for path, and some loaders

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import Post from './Post';
-const post = new Post('Webpack post title');
+import Post from '@/Post';
+import data from '@/assets/data.json';
+import WebpackLogo from '@/assets/webpack.jpg';
+import './styles/index.css';
+
+const post = new Post('Webpack post title', WebpackLogo);
 
 console.log(post);
+console.log(data);
