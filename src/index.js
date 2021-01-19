60e7d708d57471eadc495d5e85e7721fcd2de308
Alexandr <jsprog2017@yandex.ru>
2021-01-19 14:45:32 +0300

    production mode minification setttings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Post from '@/Post';
-import data from '@/assets/data.json';
-import WebpackLogo from '@/assets/webpack.jpg';
-import './styles/index.css';
+import data from '@assets/data.json';
+import WebpackLogo from '@assets/webpack.jpg';
+import '@styles/index.css';
 
 const post = new Post('Webpack post title', WebpackLogo);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,105 @@
-const path = require('path');
-const HTMLWebpackplugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const HTMLWebpackplugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        minimize: isProd,
+        splitChunks: {
+            chunks: "all",
+        },
+    };
+
+    if (isProd) {
+        config.minimizer = [new OptimizeCssAssetsPlugin(), new TerserPlugin()];
+    }
+
+    return config;
+};
+
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode: 'development',
-    entry: {
-        main: './index.js',
-        analitics: './analitics.js',
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: {
+    main: "./index.js",
+    analitics: "./analitics.js",
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@assets": path.resolve(__dirname, "src/assets"),
     },
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 8080,
+    hot: isDev,
+  },
+  plugins: [
+    new HTMLWebpackplugin({
+      template: "./index.html",
+      minify: {
+          collapseWhitespace: isProd,
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets/favicon.ico"),
+          to: path.resolve(__dirname, "dist"),
         },
-    },
-    plugins: [
-        new HTMLWebpackplugin({
-            template: './index.html',
-        }),
-        new CleanWebpackPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+            isDev ? 'style-loader' : {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: true,
+              modules: {
+                namedExport: true,
+              },
+              publicPath: (resourcePath, context) => {
+                return (
+                  path.relative(path.dirname(resourcePath), context) + "/dist/"
+                );
+              },
             },
-            {
-                test: /\.(png|jpg|svg|gif)$/,
-                use: ['file-loader'],
-            },
-            {
-                test: /\.(ttf|woff|woff2|eot)$/,
-                use: ['file-loader'],
-            },
+          },
+          "css-loader",
         ],
-    }, 
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
 };
